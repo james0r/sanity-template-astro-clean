63e75c4205ce38c938371d0b70d373bf3604ad54
James <james.auble@gmail.com>
2024-04-03 17:52:22 +0900

    fixed env files

--- studio/constants.js
+++ studio/constants.js
@@ -1,4 +1,4 @@
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:4321'
+const FRONTEND_URL = process.env.SANITY_STUDIO_FRONTEND_URL || 'http://localhost:4321'
 
 export const PREVIEWABLE_DOCUMENT_TYPES = ['post', 'post']
 
--- studio/plugins/customDocumentActions/previewLink.js
+++ studio/plugins/customDocumentActions/previewLink.js
@@ -3,13 +3,14 @@ import { PREVIEW_BASE_URL } from '../../constants'
 import { endWithSlash } from '../../utils'
 
 export default (props) => {
+  console.log('props', props)
+  console.log('PREVIEW_BASE_URL', PREVIEW_BASE_URL)
+
   return {
     label: 'Preview',
     icon: EarthGlobeIcon,
     onHandle: () => {
-      console.log(props)
-
-      window.open(`${endWithSlash(PREVIEW_BASE_URL)}${props.type}?draftId=${props.draft._id}`,'_newtab');
+      window.open(`${endWithSlash(PREVIEW_BASE_URL)}${props.type}?draftId=${props.draft._id}`,'_blank');
     },
     shortcut: 'Ctrl+Alt+E',
   }
